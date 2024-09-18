e6fc77a3bd81b1977a081971bc2630cced0192b9
Shane McKee <shane.mckee@canonical.com>
2024-09-18 23:47:09 +0800

    Added an install script for oracular

---
+++ install-oracular.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:kobuk-team/intel-graphics-testing
+sudo apt update
+sudo apt upgrade
+
+sudo apt install -y igsc intel-igc-cm libigdgmm12 libigc1 intel-level-zero-gpu-raytracing intel-media-va-driver intel-onetbb libze1 vainfo libvpl2 libvpl-tools libmetee4 intel-metrics-discovery intel-metrics-library libmfx-gen1.2 xpu-smi linux-image-unsigned-6.11.0-7-generic linux-headers-6.11.0-7-generic
+
+echo "Installation complete! Please reboot now"
+
